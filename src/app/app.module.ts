da30d66d92bfaeb015050197b62209104e2c65df
Ronan Kielt <rkielt@myges.fr>
2023-05-02 09:58:47 +0200

    component crée

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './component/counter/counter.component';
+import { ButtonComponent } from './component/button/button.component';
 
 @NgModule({
   exports: [],
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
