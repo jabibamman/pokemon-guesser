1d5c956240ca9725f5047fc22b9db66eff0c845f
James <james.bidule@gmail.com>
2023-06-29 11:17:32 +0200

    feat(entities): Add EntitiesModel properties and Enums
    - Added new properties to the EntitiesModel class, including number, name, types, height, weight, malePct, femalePct, captRate, expPoints, expSpeed, baseTotal, hp, attack, defense, special, speed,
    normalDmg,
    fireDmg,
    waterDmg,
    electricDmg,
    grassDmg,
    iceDmg,
    fightDmg,
    poisonDmg,
    groundDmg,
    flyingDmg,
    psychicDmg,
    bugDmg,
    rockDmg,
    ghostDm
    gdragon Dmgs and evolutions.
    - Created a new enum file for EntitiesTypes with water, fire grass electric normal ice poison ground flying psychic bug rock ghost dragon fighting as types.
    - Created a new enum file for ExpSpeedTypes with slow medium_slow medium_fast fast as speed types.

--- src/app/core/models/entities.model.ts
+++ src/app/core/models/entities.model.ts
@@ -1,1 +1,38 @@
-export class EntitiesModel{}
+import { EntitiesTypes } from "@shared/enums/entities-types.enum";
+import { ExpSpeedTypes } from "@shared/enums/expspeed-types.enum";
+
+export class EntitiesModel {
+    number!: number;
+    name!: string;
+    types!: EntitiesTypes[];
+    height!: number;
+    weight!: number;
+    malePct!: number;
+    femalePct!: number;
+    captRate!: number;
+    expPoints!: number;
+    expSpeed!: ExpSpeedTypes;
+    baseTotal!: number;
+    hp!: number;
+    attack!: number;
+    defense!: number;
+    special!: number;
+    speed!: number;
+    normalDmg!: number;
+    fireDmg!: number;
+    waterDmg!: number;
+    electricDmg!: number;
+    grassDmg!: number;
+    iceDmg!: number;
+    fightDmg!: number;
+    poisonDmg!: number;
+    groundDmg!: number;
+    flyingDmg!: number;
+    psychicDmg!: number;
+    bugDmg!: number;
+    rockDmg!: number;
+    ghostDmg!: number;
+    dragonDmg!: number;
+    evolutions!: number;
+    legendary!: number;
+}
