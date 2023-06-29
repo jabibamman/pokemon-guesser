e2c7b540cd719e39bbf68d9fc92a9080da431032
Ronan Kielt <rkielt@myges.fr>
2023-06-29 11:34:38 +0200

    feat: EntitiesService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {EntitiesService} from "@core/services/entities.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokemon-guesser';
+  constructor(private service: EntitiesService) {
+    this.service.getEntities();
+  }
 }
--- src/app/core/services/entities.service.ts
+++ src/app/core/services/entities.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {EntitiesModel} from "@core/models/entities.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntitiesService {
+  jsonData: any;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getEntities(): void { //EntitiesModel[] {
+    this.http.get('assets/pokemons/Data.json').subscribe(data => {
+      this.jsonData = data;
+        console.log(this.jsonData);
+    });
+  }
 }
