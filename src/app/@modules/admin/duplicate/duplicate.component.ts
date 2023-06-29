6529beb6b4d7a42e612550d07f5385584db27ba3
James <james.bidule@gmail.com>
2023-06-29 09:56:04 +0200

    feat(arch): clean architecture

---
+++ src/app/@modules/admin/duplicate/duplicate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-duplicate',
+  templateUrl: './duplicate.component.html',
+  styleUrls: ['./duplicate.component.css']
+})
+export class DuplicateComponent {
+
+}
