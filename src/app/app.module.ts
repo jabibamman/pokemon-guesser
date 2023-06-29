b187ca46386f3aaf21fe49b2137f6736bbe1564c
James <james.bidule@gmail.com>
2023-06-29 10:43:38 +0200

    fix(angular): v16 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DeleteComponent } from './@modules/admin/delete/delete.component';
-import { CreateComponent } from './@modules/admin/create/create.component';
-import { UpdateComponent } from './@modules/admin/update/update.component';
-import { DuplicateComponent } from './@modules/admin/duplicate/duplicate.component';
-import { EntitiesComponent } from './@modules/entities/entities.component';
-import { DetailsComponent } from './@modules/details/details.component';
+import { DeleteComponent } from './modules/admin/delete/delete.component';
+import { CreateComponent } from './modules/admin/create/create.component';
+import { UpdateComponent } from './modules/admin/update/update.component';
+import { DuplicateComponent } from './modules/admin/duplicate/duplicate.component';
+import { EntitiesComponent } from './modules/entities/entities.component';
+import { DetailsComponent } from './modules/details/details.component';
 
 @NgModule({
   exports: [],
